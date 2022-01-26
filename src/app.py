33a712f209cbe031e4162b375df0eddba03847d8
Konstantinos Voulgaris <konstantinos@voulgaris.info>
2022-01-26 20:59:01 +0200

    Update to SocketIO

--- src/app.py
+++ src/app.py
@@ -2,11 +2,13 @@ import os
 import io
 import uuid
 import pandas as pd
+import json
 
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_socketio import SocketIO
 from minio import Minio
 
-from parse import parse_request_form, parse_file
+from parse import parse_data_dict, parse_request_form, parse_file
 from load import load_data_as_dataframe
 from metadata import Metadata, save_metadata
 from clean import drop_null, clean_string, clean_number
@@ -19,6 +21,7 @@ MINIO_USER = os.getenv("MINIO_USER", "minioadmin")
 MINIO_PASS = os.getenv("MINIO_PASS", "minioadmin")
 
 app = Flask(__name__)
+socket = SocketIO(app)
 
 client = Minio(f"{MINIO_HOST}:{MINIO_PORT}", access_key=MINIO_USER, secret_key=MINIO_PASS, secure=False)
 
@@ -27,38 +30,38 @@ try:
 except:
     print("Failed to create connection with MinIO!")
     exit(1)
-    
+
 print("Created connection with MinIO!")
 
 
-@app.route("/data-loading", methods=["POST"])
-def data_loading():
-    input_bucket, input_path, output_bucket, output_path = parse_request_form(request)
+@socket.on("data-loading")
+def data_loading(data):
+    data_dict = json.loads(data)
+    input_bucket, input_path, output_bucket, output_path = parse_data_dict(data_dict)
 
     if not input_bucket:
-        return "Missing or invalid keys in request form!", 400
+        socket.emit("data-loading-error", {"error": "Missing data"})
+        return
 
     files = client.list_objects(input_bucket, recursive=True)
-    exports = list()
-    
     metadata = Metadata(f"Dataset", "N/A", output_path, "N/A", 0, 0, 0)
+    exports = list()
 
     for f in files:
         file_path, file_directory, file_extension = parse_file(f.object_name)
-        
+
         if file_directory != input_path:
             continue
-        
+
         data = client.get_object(input_bucket, file_path).read()
         data = io.BytesIO(data)
         data.seek(0)
-        
+
         df = load_data_as_dataframe(data, file_extension)
-        
+
         if df.empty:
-            print(f"- Skipping: '{input_bucket}/{file_path}'\n  Reason: Unknown file type!")
             continue
-        
+
         df_name = f"{output_path}/{uuid.uuid4().hex}.csv"
         df_data = df.to_csv(index=False).encode("utf-8")
         df_length = len(df_data)
@@ -74,39 +77,40 @@ def data_loading():
 
     save_metadata(metadata)
 
-    return jsonify({"loaded": exports, "metadata": metadata.to_dict()})
+    socket.emit("data-loading-done", {"loaded": exports, "metadata": metadata.to_dict(), "job-id": data_dict.get("job-id")})
 
 
-@app.route("/data-cleaning", methods=["POST"])
-def data_cleaning():
-    input_bucket, input_path, output_bucket, output_path = parse_request_form(request)
+@socket.on("data-cleaning")
+def data_cleaning(data):
+    data_dict = json.loads(data)
+    input_bucket, input_path, output_bucket, output_path = parse_data_dict(data_dict)
 
     if not input_bucket:
-        return "Missing or invalid keys in request form!", 400
+        socket.emit("data-cleaning-error", {"error": "Missing data"})
+        return
 
-    max_shrink = float(request.args.get("max-shrink", 0.2))
-    
+    max_shrink = float(data_dict.get("max-shrink", 0.2))
+
     files = client.list_objects(input_bucket, recursive=True)
     dfs = list()
 
     for f in files:
         file_path, file_directory, file_extension = parse_file(f.object_name)
-        
+
         if file_directory != input_path:
             continue
-        
+
         data = client.get_object(input_bucket, file_path).read()
         data = io.BytesIO(data)
         data.seek(0)
-        
+
         df = load_data_as_dataframe(data, file_extension)
         
         if df.empty:
-            print(f"- Skipping: '{input_bucket}/{file_path}'\n  Reason: Unknown file type!")
             continue
-        
+
         dfs.append(df)
-    
+
     df = pd.concat(dfs).reset_index(drop=True)
     df = drop_null(df, max_shrink)
     df = clean_string(df)
@@ -119,8 +123,8 @@ def data_cleaning():
     df_data.seek(0)
 
     client.put_object(output_bucket, df_name, df_data, df_length, content_type="application/csv")
-    
-    return jsonify(f"{output_bucket}/{df_name}")
+
+    socket.emit("data-cleaning-done", {"job-id": data_dict.get("job-id")})
 
 if __name__ == "__main__":
     app.run(HOST, PORT, True)
--- src/parse.py
+++ src/parse.py
@@ -1,19 +1,18 @@
-from flask import Request
 from typing import Tuple
 
 
-def parse_request_form(r: Request) -> Tuple[str, str, str, str]:
-    minio_input = r.form.get("minio-input", None)
-    minio_output = r.form.get("minio-output", None)
-    
+def parse_data_dict(data_dict: dict()) -> Tuple[str, str, str, str]:
+    minio_input = data_dict.get("minio-input")
+    minio_output = data_dict.get("minio-output")
+
     if not (minio_input and minio_output):
         return (None, None, None, None)
-    
+
     input_bucket = minio_input.split("/")[0]
     input_path = minio_input.partition("/")[-1]
     output_bucket = minio_output.split("/")[0]
     output_path = minio_output.partition("/")[-1]
-    
+
     return (input_bucket, input_path, output_bucket, output_path)
 
 
