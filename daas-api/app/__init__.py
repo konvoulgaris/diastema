e4575e061292f6979f99691da8987d89ec4db4d5
Konstantinos Voulgaris <konstantinos@voulgaris.info>
2022-03-04 12:45:23 +0200

    Diastema DaaS API basic structure

--- daas-api/app/__init__.py
+++ daas-api/app/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 
 
-def create_app() -> Flask:
+def create_app():
     app = Flask(__name__)
 
+    with app.app_context():
 
-    @app.route("/", methods=["GET"])
-    def _():
-        return "Welcome to the Diastema DaaS API!", 200
+
+        @app.route("/", methods=["GET"])
+        def index():
+            return "Welcome to the Diastema DaaS API!", 200
 
 
     return app
--- daas-api/wsgi.py
+++ daas-api/wsgi.py
@@ -4,4 +4,3 @@ wsgi = create_app()
 
 if __name__ == "__main__":
     wsgi.run("0.0.0.0", 5000, True)
-
